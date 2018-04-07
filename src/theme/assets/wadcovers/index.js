b7f01471a75f9dc7b1ebed5c547eb9aa5c25460d
PROPHESSOR <xenteze@gmail.com>
2018-04-07 11:22:03 +0300

    Добавлен загрузчик обложек вадов

---
+++ src/theme/assets/wadcovers/index.js
@@ -0,0 +1,29 @@
+import chex from './chex.jpg';
+import chex2 from './chex2.jpg';
+import doom from './doom.jpg';
+import doom2 from './doom2.jpg';
+import doom64 from './doom64.jpg';
+import freedoom from './freedoom.jpg';
+import freedoom2 from './freedoom2.jpg';
+import hacx from './hacx.jpg';
+import heretic from './heretic.jpg';
+import hexen from './hexen.jpg';
+import plutonia from './plutonia.jpg';
+import strife from './strife.jpg';
+import tnt from './tnt.jpg';
+
+export default {
+    chex,
+    chex2,
+    doom,
+    doom2,
+    doom64,
+    freedoom,
+    freedoom2,
+    hacx,
+    heretic,
+    hexen,
+    plutonia,
+    strife,
+    tnt
+};
