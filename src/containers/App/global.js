b9f3d211bd2692aa92dff0c2b92fae8b451c4b54
PROPHESSOR <xenteze@gmail.com>
2018-04-28 22:03:42 +0300

    window.DBFGL как event emitter

--- src/containers/App/global.js
+++ src/containers/App/global.js
@@ -1,3 +1,9 @@
-window.DBFGL = {
-    native: typeof window.require === 'function'
-};
+import { EventEmitter } from 'events';
+
+class GlobalClass extends EventEmitter {
+    get isNative () {
+        return typeof window.require === 'function';
+    }
+}
+
+window.DBFGL = new GlobalClass();
