b0cb2584ac2f4848ac046b0f0907c167a11dcdd0
PROPHESSOR <xenteze@gmail.com>
2018-05-04 01:03:09 +0300

    Добавил компонент секции настроек

---
+++ src/containers/Settings/Section/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import type from 'prop-types';
+
+import { Card, CardHeader, CardText } from 'material-ui/Card';
+
+export default class Section extends Component {
+  static propTypes = {
+      title:    type.string.isRequired,
+      children: type.object,
+      subtitle: type.string
+  }
+
+  state = {
+      expanded: false
+  }
+
+  handleExpandChange = () => {
+      this.setState({ expanded: !this.state.expanded });
+  }
+
+  render () {
+      const { children, title, subtitle } = this.props;
+
+      return (
+          <Card expanded = { this.state.expanded } onExpandChange = { this.handleExpandChange }>
+              <CardHeader
+                  actAsExpander
+                  showExpandableButton
+                  subtitle = { subtitle }
+                  title = { title }
+              />
+
+              <CardText expandable>
+                  {children}
+              </CardText>
+
+          </Card>
+      );
+  }
+}
