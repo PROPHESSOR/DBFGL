26cab990b3e5c775e1901be5d2e81a4fba65b9bf
PROPHESSOR <xenteze@gmail.com>
2020-03-03 18:41:42 +0200

    [WebPack] Альтернативное решение предыдущего коммита

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -272,16 +272,17 @@ module.exports = {
         // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
         // You can remove this if you don't use Moment.js:
         // new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-
-        new webpack.IgnorePlugin(/child_process/),
     ],
     // Some libraries import Node modules but don't use them in the browser.
     // Tell Webpack to provide empty mocks for them so importing them works.
     node: {
-        dgram: 'empty',
-        fs: 'empty',
-        net: 'empty',
-        tls: 'empty'
+        dgram:          'empty',
+        fs:             'empty',
+        net:            'empty',
+        tls:            'empty',
+        os:             'empty',
+        electron:       'empty',
+        child_process:  'empty',
     },
     // Turn off performance hints during development because we don't do any
     // splitting or minification in interest of speed. These warnings become
