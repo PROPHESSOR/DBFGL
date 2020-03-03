43b4ab894df07355c3098d7e03e38c6acd705e6c
PROPHESSOR <xenteze@gmail.com>
2020-03-03 17:13:00 +0200

    Добавлена поддержка ключа --devmode

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const {app, BrowserWindow} = require('electron');
+const path = require('path');
+
+const { app, BrowserWindow } = require('electron');
+
+const devmode = process.argv.includes('--devmode');
 
 app.on('ready', () => {
     const window = new BrowserWindow({
@@ -6,8 +10,11 @@ app.on('ready', () => {
         height: 768,
         autoHideMenuBar: true
     });
-    
-    window.loadURL('http://localhost:3000'); // TODO: Сделать нормально
+
+    window.loadURL(devmode ? 'http://localhost:3000' : path.join(__dirname, 'dist/index.html'));
+    // TODO: Сделать полноценную livereload интеграцию
+
+    if (devmode) window.webContents.openDevTools({ mode: 'detach' });
 });
 
 module.exports = app;
