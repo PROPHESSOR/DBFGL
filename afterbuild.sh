cc73d88e3b176f87c125d4480fcf47c6dd5ae5f3
PROPHESSOR <xenteze@gmail.com>
2020-03-04 21:41:15 +0200

    [Electron Builder] Настроен pipeline сборки

---
+++ afterbuild.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Этот скрипт нужно выполнять после yarn build.
+# Он подготовит папку build для упаковки Electron Builder
+
+cp app.js build/
+cp package-app.json build/package.json
+
+# Патч для совместимости с wadcover'ами TODO: Решить на уровне приложения
+mkdir build/public
+mv build/assets build/public/
