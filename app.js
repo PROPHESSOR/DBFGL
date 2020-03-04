04e4ba4b0dcae28204efa060ed6ba9b9b57cd245
PROPHESSOR <xenteze@gmail.com>
2020-03-04 21:20:42 +0200

    Оптимизирован app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { app, BrowserWindow } = require('electron');
 
 const devmode = process.argv.includes('--devmode');
 const livereload = process.argv.includes('--livereload');
+
 /**
  * Открывает localhost:3000
  * Делает недоступными require функции
@@ -13,26 +14,15 @@ const oldlivereload = process.argv.includes('--oldlivereload');
 
 app.on('ready', () => {
     const window = new BrowserWindow({
-        width: 1024,
-        height: 768,
-        autoHideMenuBar: true
+        width:           1024,
+        height:          768,
+        autoHideMenuBar: true,
     });
 
-    if (oldlivereload) {
-        window.loadURL('http://localhost:3000');
-    } else if (livereload) {
-        window.loadURL(url.format({
-            pathname: path.join(__dirname, 'electron_live.html'),
-            protocol: 'file:',
-            slashes: true
-        }));
-    } else {
-        window.loadURL(url.format({
-            pathname: path.join(__dirname, 'build/index.html'),
-            protocol: 'file:',
-            slashes: true
-        }));
-    }
+    if (oldlivereload) window.loadURL('http://localhost:3000');
+    else if (livereload) window.loadFile('electron_live.html');
+    else window.loadFile(path.join('index.html'));
+
 
     if (devmode) window.webContents.openDevTools({ mode: 'detach' });
 });
