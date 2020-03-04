c9e7f17ef4fe3451ea331b605c35df5fa27134b8
PROPHESSOR <xenteze@gmail.com>
2020-03-04 14:17:30 +0200

    Исправлено скрытие/отображение лаунчера в Linux

--- src/Global.js
+++ src/Global.js
@@ -24,12 +24,18 @@ class GlobalClass extends EventEmitter {
 
         this.on('window.minimize', () => {
             if (!isNative) return;
-            electron.remote.BrowserWindow.getFocusedWindow().minimize();
+            if (this.os === 'Linux')
+                electron.remote.BrowserWindow.getFocusedWindow().hide();
+            else
+                electron.remote.BrowserWindow.getFocusedWindow().minimize();
         });
 
         this.on('window.restore', () => {
             if (!isNative) return;
-            electron.remote.BrowserWindow.getAllWindows().forEach(win => win.restore());
+            if (this.os === 'Linux')
+                electron.remote.BrowserWindow.getAllWindows().forEach(win => win.show());
+            else
+                electron.remote.BrowserWindow.getAllWindows().forEach(win => win.restore());
         });
     }
 
@@ -41,6 +47,27 @@ class GlobalClass extends EventEmitter {
 
     get isNative () {
         return isNative;
+    }
+
+    /**
+     * @returns {"Browser"|"Windows"|"Linux"|"Mac"|"Unknown"}
+     */
+    get os() {
+        if (!isNative) return 'Browser';
+
+        const os = require('os').type();
+
+        switch (os) {
+            case 'Linux':
+                return 'Linux';
+            case 'Darwin':
+                return 'Mac';
+            case 'Windows_NT':
+                return 'Windows';
+            default:
+                console.warn(`[Global::os] Unknown platform ${os}!`);
+                return 'Unknown';
+        }
     }
 
     get appData () {
