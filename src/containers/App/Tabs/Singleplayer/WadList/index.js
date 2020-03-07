fd86eefa3db2032e62a50d0133721c1c6e541688
PROPHESSOR <xenteze@gmail.com>
2020-03-07 20:29:00 +0200

    [Singleplayer] [WadList] Перенесён на Redux

--- src/containers/App/Tabs/Singleplayer/WadList/index.js
+++ src/containers/App/Tabs/Singleplayer/WadList/index.js
@@ -1,76 +1,50 @@
 import React, { Component } from 'react';
 import type from 'prop-types';
 
-import DBFGL from '@/Global';
-
 import { List } from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 
 import Wad from './wad'; // Component
 // eslint-disable-next-line no-unused-vars
 import DoomFile from '@/classes/DoomFile'; // Class
-import { getZDoomLaunchFilesWithoutIWads } from '@/utils/getWadsFromFs';
+import { connect } from 'react-redux';
+import { createBindStateToProps, createBindActToProps, storeProps } from '@/store';
 
-export default class WadList extends Component {
-    static propTypes = {
-        style: type.object,
+export default connect(
+    createBindStateToProps('singleplayer.wadlist', 'singleplayer.selected'),
+    createBindActToProps()
+)(
+    class WadList extends Component {
+        static propTypes = {
+            ...storeProps,
+            wadlist:  type.array.isRequired,
+            selected: type.array.isRequired,
+            style:    type.object,
+        }
+
+        /**
+         * @param {DoomFile} wad
+         */
+        onSelect = wad => { // (wad, index)
+            const { act, actions } = this.props;
+
+            act(actions.singleplayer_wadlist_selected_add, wad);
+        }
+
+        render() {
+            const selected = new Set(this.props.selected);
+
+            const jsxwads = this.props.wadlist
+                .filter(wad => !selected.has(wad))
+                .map(wad => (<Wad key={wad.name} wad={wad} onClick={this.onSelect} />));
+
+            return (
+                <List
+                    style={{ ...this.props.style, height: '-webkit-fill-available' }}>
+                    <Subheader>Все WAD&apos;ы:</Subheader>
+                    {jsxwads}
+                </List>
+            );
+        }
     }
-
-    constructor() {
-        super();
-        this.state = {
-            selectedIndex: 0,
-
-            /**
-             * @type {Array<DoomFile>}
-             */
-            wads: [],
-        };
-        DBFGL.on('singleplayer.wadlist.update', this.updateWads);
-        DBFGL.on('singleplayer.wadlist.selected.update', this.forceUpdate); // FIXME: Используется для отлавливания изменений в WadClass.selected
-    }
-
-    componentDidMount = () => {
-        this.updateWads();
-    }
-
-    componentWillUnmount() {
-        DBFGL.removeListener('singleplayer.wadlist.update', this.updateWads);
-        DBFGL.removeListener('singleplayer.wadlist.selected.update', this.forceUpdate);
-    }
-
-    updateWads = () => {
-        this.setState({ // FIXME: Мне кажется, или здесь дикий перерасход ресурсов? :/
-            wads: getZDoomLaunchFilesWithoutIWads(), //.map((name) => new WadClass({ name }))
-        });
-        console.log('Список вадов обновлен');
-    }
-
-    forceUpdate = () => super.forceUpdate();
-
-    /**
-     * @param {DoomFile} wad
-     */
-    onSelect = wad => { // (wad, index)
-        // console.log('onselect', wad, index);
-        wad.selected = true;
-        DBFGL.singleplayer.selected.push(wad);
-        DBFGL.emit('singleplayer.wadlist.selected.update');
-    }
-
-    render() {
-        const jsxwads = this.state.wads
-            .filter(wad => !wad.selected)
-            .map((wad, i) => (<Wad key={wad.name} value={i} wad={wad} onClick={this.onSelect} />));
-
-        return (
-            <List
-                style={{ ...this.props.style, height: '-webkit-fill-available' }}
-                value={this.state.selectedIndex} // FIXME: Зачем?
-                onChange={this.onSelect}>
-                <Subheader>Все WAD&apos;ы:</Subheader>
-                {jsxwads}
-            </List>
-        );
-    }
-}
+);
