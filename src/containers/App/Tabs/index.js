8ed7f901da7f2db80d389ce5f46dbe3a6b40492f
PROPHESSOR <xenteze@gmail.com>
2020-03-07 18:50:10 +0200

    Вернул табы к изначальному состоянию
    Они являются автономным компонентом, который посылает события,
    так что, изначальная событийная модель здесь подходит как никак кстати

--- src/containers/App/Tabs/index.js
+++ src/containers/App/Tabs/index.js
@@ -4,52 +4,51 @@ import SwipeableViews from 'react-swipeable-views';
 
 import { Tabs, Tab } from 'material-ui/Tabs';
 
+import DBFGL from '@/Global';
+
 import Singleplayer from './Singleplayer';
 import Multiplayer from './Multiplayer';
-import { connect } from 'react-redux';
-import { createBindStateToProps, createBindActToProps, storeProps } from '@/store';
 
-const indexToString = index => index === 0 ? 'singleplayer' : 'multiplayer';
-const stringToIndex = string => string === 'singleplayer' ? 0 : 1;
-
-export default connect(
-    createBindStateToProps('interface.tab'),
-    createBindActToProps(),
-)(
-    class DBFGLTabs extends Component {
-        static propTypes = {
-            ...storeProps,
-            tab:       type.number.isRequired,
-            toggleTab: type.func,
-        }
-
-        handleChange = index => this.props.act(this.props.actions.interface_tab_change, indexToString(index));
-
-        render() {
-            const { tab } = this.props;
-            const index = stringToIndex(tab);
-
-            return (<div>
-                <Tabs
-                    value={index}
-                    onChange={this.handleChange}>
-                    <Tab label='Одиночная игра' value={0} />
-                    <Tab label='Мультиплеер' value={1} />
-                </Tabs>
-                <SwipeableViews
-                    index={index}
-                    slideStyle={{
-                        overflow: 'auto',
-                    }}>
-                    <div>
-                        <Singleplayer />
-                    </div>
-                    <div>
-                        <Multiplayer />
-                    </div>
-                </SwipeableViews>
-            </div>
-            );
-        }
+export default class DBFGLTabs extends Component {
+    static propTypes = {
+        toggleTab: type.func,
     }
-);
+
+    state = {
+        slideIndex: 0,
+    }
+
+    handleChange = value => {
+        this.setState({
+            slideIndex: value,
+        });
+
+        const val = value ? 'multiplayer' : 'singleplayer';
+
+        return DBFGL.emit('tab.change', val);
+    };
+
+    render() {
+        return (<div>
+            <Tabs
+                value={this.state.slideIndex}
+                onChange={this.handleChange}>
+                <Tab label='Одиночная игра' value={0} />
+                <Tab label='Мультиплеер' value={1} />
+            </Tabs>
+            <SwipeableViews
+                index={this.state.slideIndex}
+                slideStyle={{
+                    overflow: 'auto',
+                }}>
+                <div>
+                    <Singleplayer />
+                </div>
+                <div>
+                    <Multiplayer />
+                </div>
+            </SwipeableViews>
+        </div>
+        );
+    }
+}
--- src/store/interface.js
+++ src/store/interface.js
@@ -1,27 +1,17 @@
 /* eslint-disable camelcase, default-case */
 
 const store = {
-
-    /**
-     * @type {"singleplayer"|"multiplayer"}
-     */
-    tab: 'singleplayer',
 };
 
 export const actions = {
-    interface_tab_change: 'interface.tab.change',
 };
 
 
 export function reducer(state = store, action) {
-    const { type, payload } = action;
+    // const { type, payload } = action;
 
-    switch (type) {
-        case actions.interface_tab_change:
-            return {
-                tab: payload,
-            };
-    }
+    // switch (type) {
+    // }
 
 
     return state;
