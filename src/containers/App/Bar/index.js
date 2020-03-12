70ab8d5f24bc1c108e8622fac5a4fa84c4df8879
PROPHESSOR <xenteze@gmail.com>
2020-03-12 22:22:07 +0200

    [Bar] Добавлена кнопка обновления вкладки

--- src/containers/App/Bar/index.js
+++ src/containers/App/Bar/index.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import DBFGL from '@/Global';
 
 // Components
-import { AppBar } from 'material-ui';
+import { AppBar, IconButton } from 'material-ui';
+import { NavigationRefresh } from 'material-ui/svg-icons';
 
 export default class Bar extends Component {
     togglePanel = mode => {
         if (mode) DBFGL.emit('panel.open', 'left');
         else DBFGL.emit('panel.close', 'left');
+    }
 
+    update = () => {
+        DBFGL.emit(`${DBFGL.tab}.update`);
     }
 
     render() {
         return (
             <AppBar
                 title='DooM BFG Launcher'
+                iconElementRight={<IconButton><NavigationRefresh /></IconButton>}
                 onLeftIconButtonClick={this.togglePanel}
+                onRightIconButtonClick={this.update}
             />
         );
     }
