c58f7980acf27772af1bfef1b7839855905fa012
PROPHESSOR <xenteze@gmail.com>
2020-04-12 10:48:47 +0300

    [Redux] Добавлено поле для выбранного серера

--- src/store/multiplayer.js
+++ src/store/multiplayer.js
@@ -28,12 +28,14 @@ const store = {
      * @type {Array<ServerInstance>}
      */
     serverlist: [],
+    selected:   null,
 };
 
 export const actions = {
     multiplayer_serverlist_update:        'multiplayer.serverlist.update',
     multiplayer_serverlist_add:           'multiplayer.serverlist.add',
     multiplayer_serverlist_server_update: 'multiplayer.serverlist.server.update', //{ipport, data}
+    multiplayer_serverlist_select:        'multiplayer.serverlist.select',
 };
 
 /**
@@ -62,7 +64,7 @@ export function reducer(state = store, action) {
         case actions.multiplayer_serverlist_add:
             if (!payload || payload instanceof Array) throw new Error('You must specify a server to add');
 
-            return { ...state, serverlist: sortServerlist([...state.serverlist, payload])};
+            return { ...state, serverlist: sortServerlist([...state.serverlist, payload]) };
 
         case actions.multiplayer_serverlist_server_update: {
             const { index, diff } = payload;
@@ -71,6 +73,12 @@ export function reducer(state = store, action) {
 
             return { ...state, serverlist: sortServerlist(state.serverlist.map(server => `${server.ip}:${server.port}` === index ? { ...server, ...diff } : server)) };
         }
+
+        case actions.multiplayer_serverlist_select:
+            if (!(typeof payload === 'number')) throw new Error('Selected server id must be a number!');
+            if (payload < 0 || payload >= state.serverlist.length) throw new Error(`Selected id must be > 0 and < ${state.serverlist.length}!`);
+
+            return { ...state, selected: payload };
     }
 
     return state;
